8e99d99398dc3d61e3e60571f64000b73cdefe34
MashaalSayeed <mashaal2403@gmail.com>
2024-09-29 23:46:27 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from mangum import Mangum
+
+app = FastAPI()
+handler = Mangum(app)
+
+drone_status = {"state": "standby"}
+
+class CommandRequest(BaseModel):
+    command: str
+
+@app.post("/command")
+def command(request: CommandRequest):
+    command = request.command
+
+    if command not in ["fly", "standby"]:
+        raise HTTPException(status_code=400, detail="Invalid command")
+
+    drone_status['state'] = command
+    return {"status": "success", "command": command}
+
+
+@app.get("/status")
+def status():
+    return drone_status
